7203e4e72b6981f9be6670a81d3c77e3c2b1ebc3
Gnanendhra <87071227+Gnanendhra@users.noreply.github.com>
2023-05-15 22:25:11 +0530

    Revert back changes

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef } from "react";
 
 import emailjs from "@emailjs/browser";
 
@@ -6,60 +6,34 @@ import "./ContactMe.css";
 
 import { contactDetails } from "../../data/contacts";
 
-const ContactMe = () => {
+const ContactMe = (e) => {
   const form = useRef();
-  const [inputValues, setInputValues] = useState({
-    user_name: "",
-    user_email: "",
-    subject: "",
-    message: ""
-  });
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const isAnyValueEmpty = Object.values(inputValues).some(value => value === "");
-    const sendButton = form.current.querySelector(".hire-btn");
-    sendButton.disabled = isAnyValueEmpty;
-  }, [inputValues]);
-
-  const handleInputChange = (e) => {
-    setInputValues({ ...inputValues, [e.target.name]: e.target.value });
-  };
-
   const sendEmail = (e) => {
     e.preventDefault();
-
-    setIsLoading(true);
 
     emailjs
       .sendForm(
         process.env.REACT_APP_SERVICE_ID,
         process.env.REACT_APP_TEMPLATE_ID,
         form.current,
-        process.env.REACT_APP_USER_ID
+        process.env.REACT_APP_PUBLIC_KEY
       )
       .then(
         (result) => {
           console.log(result.text);
-          setIsLoading(false);
-          setInputValues({
-            user_name: "",
-            user_email: "",
-            subject: "",
-            message: ""
-          });
         },
         (error) => {
           console.log(error.text);
-          setIsLoading(false);
         }
       );
+    e.target.reset();
   };
 
   return (
     <div id="contact" className="reachme-container">
       <div className="reachme-title2">
         <h2>I Want To Hear From You</h2>
+
         <h3>Contact Me</h3>
       </div>
       <div className="row">
@@ -68,7 +42,7 @@ const ContactMe = () => {
             <div className="row">
               {contactDetails &&
                 contactDetails.map((details) => (
-                  <div className="contact-info" key={details.id}>
+                  <div className="contact-info  " key={details.id}>
                     <div className="contact-details">
                       <i className={details.icon}></i>
                       <div className="contact-mi">
@@ -90,24 +64,20 @@ const ContactMe = () => {
           >
             <div className="row">
               <div className="col-md-12 mb-3 hire-me-title"></div>
-              <div className="col-md-6">
+              <div className="col-md-6 ">
                 <input
                   type="text"
                   name="user_name"
                   id=""
                   placeholder="Enter Your Name"
-                  value={inputValues.user_name}
-                  onChange={handleInputChange}
                 />
               </div>
-              <div className="col-md-6">
+              <div className="col-md-6 ">
                 <input
                   type="email"
                   name="user_email"
                   id=""
                   placeholder="Enter Your Email"
-                  value={inputValues.user_email}
-                  onChange={handleInputChange}
                 />
               </div>
               <div className="col-md-12">
@@ -116,10 +86,8 @@ const ContactMe = () => {
                   name="subject"
                   id=""
                   placeholder="Enter Subject"
-                  value={inputValues.subject}
-                  onChange={  handleInputChange}
-                  />
-                   </div>
+                />
+              </div>
               <div className="col-md-12 mb-2">
                 <textarea
                   name="message"
@@ -127,11 +95,9 @@ const ContactMe = () => {
                   cols="60"
                   rows="8"
                   placeholder="Your Message"
-                  value={inputValues.message}
-                  onChange={handleInputChange}
                 ></textarea>
-                <button className="hire-btn" type="submit" disabled={isLoading}>
-                  {isLoading ? "Sending..." : "Send Message"}
+                <button className="hire-btn" type="submit">
+                  Send Message
                 </button>
               </div>
             </div>
