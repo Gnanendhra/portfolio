e769d8c8aedc40d0fd09e055184d30477ef8bf13
Gnanendhra <87071227+Gnanendhra@users.noreply.github.com>
2023-05-15 21:41:47 +0530

    Updated Content

--- src/data/experience.js
+++ src/data/experience.js
@@ -2,7 +2,7 @@ export const experienceData = {
     "workExperience": [
         {
             id: 1,
-            company: 'Hexagon',
+            company: 'Hexagon Capability Center',
             companyLogo: 'https://www.hexagon.com/images/v-2-logo.svg/-/media/project/one-web/master-site/defaults/icons/hexagon-logo.png?h=59&iar=0&w=192&hash=81E2D76B11580C47B0992E91EDE3722E',
             jobtitle: 'Software Developer',
             description: "Contributed to the OCR (On Call Records) team by designing and developing UI components and gave backend support with ReactJS and .Net.",
@@ -10,8 +10,8 @@ export const experienceData = {
             icon: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'
         },
         {
-            id: 4,
-            company: 'Cognizant',
+            id: 2,
+            company: 'Cognizant Technology Solutions',
             companyLogo: 'https://www.cognizant.com/content/dam/connectedassets/cognizant-global-marketing/marketing-channels/cognizant-dotcom/en_us/logos/cognizant/COG-Logo-2022.svg',
             jobtitle: 'Programmer Analyst Trainee',
             description: "Trained and deployed into project, created file to file scenario with data/message types, service interface, and mappings.",
@@ -21,7 +21,7 @@ export const experienceData = {
     ],
     "studies": [
         {
-            id: 5,
+            id: 3,
             institution: 'Aditya College Of Engineering',
             course: 'Bachelor\'s degree in Computer Science',
             date: '2016 - 2020',
@@ -29,7 +29,7 @@ export const experienceData = {
             icon: 'https://img.icons8.com/?size=512&id=38833&format=png',
         },
         {
-            id: 6,
+            id: 4,
             institution: 'Bharathi Junior College',
             course: 'Intermediate',
             date: '2014 - 2016',
@@ -37,7 +37,7 @@ export const experienceData = {
             icon: 'https://img.icons8.com/?size=512&id=38833&format=png',
         },
         {
-            id: 7,
+            id: 5,
             institution: 'Goutham High School',
             course: 'Seconday Education',
             date: '2014',
