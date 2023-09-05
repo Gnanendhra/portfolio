696bc1fe157b30d897e29b65b13a60dff5d0408d
Gnanendhra <gnanendhrakosuri@gmail.com>
2023-09-05 21:35:04 +0530

    Update Role

--- src/data/aboutMe.js
+++ src/data/aboutMe.js
@@ -2,7 +2,7 @@ export const aboutMeData = [
     {
         "id": 1,
         "title": "Who I am",
-        "description_one": "<p>I'm a creative <strong>SOFTWARE DEVELOPER</strong><strong> </strong>based in Hyderabad, India, with 25 years of age, specializing in User Interface Design and Development. </p>\r\n\r\n<p>Currently, I hold the position of a <strong>Software Developer</strong> at <a href=\"https://www.hexagon.com/\" target=\"_blank\"><strong>Hexagon</strong></a>. My expertise lies in creating clean, visually appealing, and fully functional interfaces that adhere to the most up-to-date web standards🔥<span style=\"color:#2ecc71\">.</span></p><p>Currently, I'm also delving into <strong>NextJS</strong>, as I have a deep love for learning and exploring new domains in the field of technology.</p>",
+        "description_one": "<p>I'm a creative <strong>SENIOR SOFTWARE DEVELOPER</strong><strong> </strong>based in Hyderabad, India, with 25 years of age, specializing in User Interface Design and Development. </p>\r\n\r\n<p>Currently, I hold the position of a <strong>Senior Software Developer</strong> at <a href=\"https://www.hexagon.com/\" target=\"_blank\"><strong>Hexagon</strong></a>. My expertise lies in creating clean, visually appealing, and fully functional interfaces that adhere to the most up-to-date web standards🔥<span style=\"color:#2ecc71\">.</span></p><p>Currently, I'm also delving into <strong>NextJS</strong>, as I have a deep love for learning and exploring new domains in the field of technology.</p>",
         "about_avatar": "1B97-7cbBT2HhLUqHh5bvfckaGLsudQmj"
     }
 ]
--- src/data/experience.js
+++ src/data/experience.js
@@ -4,7 +4,7 @@ export const experienceData = {
             id: 1,
             company: 'Hexagon Capability Center',
             companyLogo: 'hexagon',
-            jobtitle: 'Software Developer',
+            jobtitle: 'Senior Software Developer',
             description: "I actively participated in the development of the OCR (On Call Records) product, where my contributions encompassed developing UI components utilizing ReactJS. Additionally, I offered valuable backend support utilizing .Net and Sql technologies.",
             date: 'Dec 2021 - Present',
             icon: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'
