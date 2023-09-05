6565452cb19c4723fba37e3709be5116c68589b8
Gnanendhra <gnanendhrakosuri@gmail.com>
2023-09-05 20:41:44 +0530

    Update Role

---
+++ src/data/experience.js
@@ -0,0 +1,74 @@
+export const experienceData = {
+    "workExperience": [
+        {
+            id: 1,
+            company: 'Hexagon Capability Center',
+            companyLogo: 'hexagon',
+            jobtitle: 'Senior Software Developer',
+            description: "I actively participated in the development of the OCR (On Call Records) product, where my contributions encompassed developing UI components utilizing ReactJS. Additionally, I offered valuable backend support utilizing .Net and Sql technologies.",
+            date: 'Dec 2021 - Present',
+            icon: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'
+        },
+        {
+            id: 2,
+            company: 'Cognizant Technology Solutions',
+            companyLogo: 'cognizant',
+            jobtitle: 'Programmer Analyst Trainee',
+            description: "Trained and deployed into project, created file to file scenario with data/message types, service interface, and mappings.",
+            date: 'Dec 2020 - Nov 2021',
+            icon: 'https://img.icons8.com/?size=512&id=kOPTH4LnJoIU&format=png'
+        },
+    ],
+    "studies": [
+        {
+            id: 3,
+            institution: 'Aditya College Of Engineering',
+            course: 'Bachelor\'s degree in Computer Science',
+            date: '2016 - 2020',
+            description: 'For my academic pursuit, I have chosen the CSE branch as my major and achieved an impressive 77% in it.',
+            icon: 'https://img.icons8.com/?size=512&id=PJleCo3so0R2&format=png',
+        },
+        {
+            id: 4,
+            institution: 'Bharathi Junior College',
+            course: 'Intermediate',
+            date: '2014 - 2016',
+            description: 'I have successfully completed my Intermediate education in the MPC (Mathematics, Physics, and Chemistry) stream and achieved an outstanding score of 96%.',
+            icon: 'https://img.icons8.com/?size=512&id=PJleCo3so0R2&format=png',
+        },
+        {
+            id: 5,
+            institution: 'Goutham High School',
+            course: 'Seconday Education',
+            date: '2014',
+            description: 'I attained a remarkable 88% in the SSC examination, showcasing my academic excellence and diligent work ethic.',
+            icon: 'https://img.icons8.com/?size=512&id=PJleCo3so0R2&format=png',
+        },
+    ]
+}
+
+
+
+export const experienceStyles = {
+    icon: {
+        background: "#23234d",
+        color: "#fff",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    content: {
+        background: '#14143a',
+        color: '#ffffff'
+    },
+    arrowStyle: {
+        borderRight: "7px solid #677483"
+    },
+    iconLight: {
+        background: "#dce1eb",
+        color: "#fff",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    }
+}
