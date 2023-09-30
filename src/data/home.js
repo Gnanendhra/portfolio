06b790c9c8df9b825fe90e9dffc31a88fd471d9f
Gnanendhra <87071227+Gnanendhra@users.noreply.github.com>
2023-09-30 10:19:44 +0530

    Updated experience

--- src/data/home.js
+++ src/data/home.js
@@ -4,7 +4,7 @@ export const homeDetails = [
         "greeting": "Hello",
         "name": "Gnanendhra Kosuri",
         "job_title": "Senior Software Developer",
-        "par_inro": "I have more than 2.5 years of experience as a Web Developer, specializing in Product development. During this time, I have gained expertise in designing and developing web applications using ReactJs. Additionally, I have also been exposed to working with .NET.",
+        "par_inro": "I have almost 3 years of experience as a Web Developer, specializing in Product development. During this time, I have gained expertise in designing and developing web applications using ReactJs. Additionally, I have also been exposed to working with .NET.",
         "avatar_img": "1iyVyaGyw5HniEugxd1-qZ54rpFpn2UTc",
         "hireMe_link": "gnanendhrakosuri@gmail.com",
         "cv_link": ''
